ee0d80342bbf41d84e179163eebc0460643638c1
fduroni <fabrizio.duroni@gmail.com>
2021-03-24 09:12:03 +0100

    Removed useless view :fire:

--- Sources/ModalTabBarItem.swift
+++ Sources/ModalTabBarItem.swift
@@ -7,28 +7,15 @@
 
 import SwiftUI
 
-public struct ModalTabBarItemContent: View {
-    public var body: some View {
-        Image(systemName: "plus.circle.fill")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 55, height: 55, alignment: .center)
-            .foregroundColor(Color(.systemBlue))
-            .background(Color(.white))
-            .cornerRadius(55/2)
-            .overlay(RoundedRectangle(cornerRadius: 55/2).stroke(Color(.blue), lineWidth: 2))
-    }
-}
-
 public struct ModalTabBarItem: View {
     let modalTabBarItemContent: AnyView
     let action: () -> Void
-    
+
     public init(modalTabBarItemContent: AnyView, action: @escaping () -> Void) {
         self.modalTabBarItemContent = modalTabBarItemContent
         self.action = action
     }
-    
+
     public var body: some View {
         Group {
             VStack(spacing: 0) {
@@ -41,15 +28,15 @@ public struct ModalTabBarItem: View {
 
 struct ShowModalTabBarItem_Previews: PreviewProvider {
     static var previews: some View {
-        ModalTabBarItem(modalTabBarItemContent: AnyView(Image(systemName: "plus.circle.fill")
-                                                            .resizable()
-                                                            .aspectRatio(contentMode: .fit)
-                                                            .frame(width: 55, height: 55, alignment: .center)
-                                                            .foregroundColor(Color(.systemBlue))
-                                                            .background(Color(.white))
-                                                            .cornerRadius(55/2)
-                                                            .overlay(RoundedRectangle(cornerRadius: 55/2).stroke(Color(.blue), lineWidth: 2)))) {
-            
-        }
+        ModalTabBarItem(modalTabBarItemContent: AnyView(
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 55, height: 55, alignment: .center)
+                                .foregroundColor(Color(.systemBlue))
+                                .background(Color(.white))
+                                .cornerRadius(55/2)
+                                .overlay(RoundedRectangle(cornerRadius: 55/2).stroke(Color(.blue), lineWidth: 2)))
+        ) { }
     }
 }
