29e2c9d9c663a98b2fc59223b367aada970f9618
fduroni <fabrizio.duroni@gmail.com>
2021-03-28 15:22:00 +0200

    Add conf to swift package :sparkles:

--- DemoSPM/DemoSPM/ContentView.swift
+++ DemoSPM/DemoSPM/ContentView.swift
@@ -6,11 +6,54 @@
 //
 
 import SwiftUI
+import TabBarUIAction
 
 struct ContentView: View {
+    @State private var currentTab: TabPosition = .tab1
+    @State private var showModal: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TabBarUIAction(
+                currentTab: $currentTab,
+                showModal: $showModal,
+                colors: Colors(
+                    tabBarColor: Color(.white),
+                    tabItemColor: Color(.black),
+                    tabItemSelectionColor: Color(.blue)
+                )
+            ) {
+                TabScreen(
+                    tabItem: TabItemContent(
+                        systemImageName: "folder.badge.plus",
+                        text: "Tab Item 1",
+                        font: Font.system(size: 16)
+                    )
+                ) {
+                    Screen(text: "Screen 1", color: Color(.systemBlue))
+                }
+                TabModal {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 55, height: 55, alignment: .center)
+                        .foregroundColor(Color(.systemBlue))
+                        .background(Color(.white))
+                        .cornerRadius(55/2)
+                        .overlay(RoundedRectangle(cornerRadius: 55/2).stroke(Color(.blue), lineWidth: 2))
+                        .accessibility(identifier: "TabBarModalButton")
+                } content: {
+                    ScreenModal(showModal: $showModal)
+                }
+                TabScreen(
+                    tabItem: TabItemContent(systemImageName: "gear", text: "Tab Item 2", font: Font.system(size: 16))
+                ) {
+                    Screen(text: "Screen 2", color: Color(.systemRed))
+                }
+            }
+        }
+        .accessibilityElement(children: .contain)
+        .accessibility(identifier: "TabBarWith3Elements")
     }
 }
 
--- Package.swift
+++ Package.swift
@@ -5,5 +5,8 @@ let package = Package(
     name: "TabBarUIAction",
     products: [.library(name: "TabBarUIAction", targets: ["TabBarUIAction"])],
     dependencies: [],
-    targets: [.target(name: "TabBarUIAction", dependencies: [], path: "./Source")]
+    targets: [.target(name: "TabBarUIAction", dependencies: [], path: "./Source")],
+    platforms: [
+        .iOS(.v14)
+    ],
 )
