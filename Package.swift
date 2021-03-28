1f89e9b46a4ae3695d68543225b223ed765d7fcd
fduroni <fabrizio.duroni@gmail.com>
2021-03-28 15:26:55 +0200

    Fix swift package :sparkles:

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "TabBarUIAction",
-    products: [.library(name: "TabBarUIAction", targets: ["TabBarUIAction"])],
-    dependencies: [],
-    targets: [.target(name: "TabBarUIAction", dependencies: [], path: "./Source")],
     platforms: [
         .iOS(.v14)
     ],
+    products: [.library(name: "TabBarUIAction", targets: ["TabBarUIAction"])],
+    dependencies: [],
+    targets: [.target(name: "TabBarUIAction", dependencies: [], path: "./Source")],
     swiftLanguageVersions: [.v5]
 )
